04cce8259662827d38596e22336df13d648d94f3
Kevyn <dosreiskevyn@gmail.com>
2024-11-11 21:14:29 -0300

    Estilizando e terminando o footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,37 +1,61 @@
-import face from "../../assets/face.png"
-import insta from "../../assets/insta.png"
-import twitter from "../../assets/twitter.png"
-import style from "../../components/footer/footer.module.css"
-
+import face from "../../assets/face.png";
+import insta from "../../assets/insta.png";
+import twitter from "../../assets/twitter.png";
+import style from "../../components/footer/footer.module.css";
+import digital from "../../assets/digital.png";
 
 function Footer() {
-
-    const teste = {
-        function: 'asdfgh'
-    }
-    return (
-        <>
-            <div className={style.footer}>
-                <div>
-                    <div>
-                        <img src=""alt="" />
-                        digital college</div>
-                    <div className={style.description}>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit,<br/> 
-                        sed do eiusmod tempor incididunt ut labore et dolore.</div>
-                    <div className={style.icons}>
-                        <img src={face} alt="" />
-                        <img src={insta} alt="" />
-                        <img src={twitter} alt="" />
-                    </div>
-                </div>
-                <div></div>
-                <div></div>
-                <div></div>
-            </div>
-            <div></div>
-        </>
-    );
+  const teste = {
+    function: "asdfgh",
+  };
+  return (
+    <>
+      <div className={style.footer}>
+        <div>
+          <div className={style.slogan}>
+            <img src={digital} alt="" />
+            <div className={style.text}>digital college</div>
+          </div>
+          <div className={style.description}>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+            <br />
+            sed do eiusmod tempor incididunt ut labore et dolore.
+          </div>
+          <div className={style.icons}>
+            <img src={face} alt="" />
+            <img src={insta} alt="" />
+            <img src={twitter} alt="" />
+          </div>
+        </div>
+        <div className={style.information}>
+          <ul>Informação</ul>
+          <li>Sobre Drip Store</li>
+          <li>Segurança</li>
+          <li>Wishlist</li>
+          <li>Blog</li>
+          <li>Trabalhe Conosco</li>
+          <li>Meus Pedidos</li>
+        </div>
+        <div className={style.products}>
+          <ul>Categorias</ul>
+          <li>Camisetas</li>
+          <li>Calças</li>
+          <li>Bonés</li>
+          <li>Headphones</li>
+          <li>Ténis</li>
+        </div>
+        <div className={style.contact}>
+          <ul>Contato</ul>
+          <li>
+            Av. Santos Dumont, 1510 - 1 andar - Aldeota, Fortaleza -CE,
+            60150-161
+          </li>
+          <li>(85) 3051-3411</li>
+        </div>
+      </div>
+      <div className={style.name}>@ 2022 Digital College</div>
+    </>
+  );
 }
 
-export default Footer
+export default Footer;
